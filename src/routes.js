ff0687280b0b5cee70aefdff70b579f80d25c05b
Prakhar Saxena <prakhar.saxena@ubimaster.de>
2023-07-13 21:26:22 +0200

    updated router

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,23 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Page from './components/page';
 
 const Router = (props) => {
-  return <Routes>
-    <Route path="/" element={<div>index</div>} />
-    {props.rooms.map((room, idx) => 
-      <Route 
-        key={idx} 
-        path={"/rooms/"+room.name.replace(' ','-')} 
-        element={<Page room={room} />} 
-      />
-    )}
-  </Routes>
+  return (
+    <BrowserRouter>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<div>index</div>} />
+          {props.rooms.map((room, idx) => 
+            <Route 
+              key={idx} 
+              path={"/rooms/"+room.name.replace(' ','-')} 
+              element={<Page room={room} />} 
+            />
+          )}
+        </Routes>
+      </HashRouter>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
