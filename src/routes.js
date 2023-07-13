fcf0222bc4ff43bce7c88707a55b13cb9aa0fbf7
Prakhar Saxena <prakhar.saxena@ubimaster.de>
2023-07-13 16:40:32 +0200

    updated router

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,23 @@
+import React from "react";
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Page from './components/page';
 
 const Router = (props) => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<div>index</div>} />
-        {props.rooms.map((room, idx) => 
-          <Route 
-            key={idx} 
-            path={"/rooms/"+room.name.replace(' ','-')} 
-            element={<Page room={room} />} 
-          />
-        )}
-      </Routes>
-    </HashRouter>
+    <React.StrictMode>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<div>index</div>} />
+          {props.rooms.map((room, idx) => 
+            <Route 
+              key={idx} 
+              path={"/rooms/"+room.name.replace(' ','-')} 
+              element={<Page room={room} />} 
+            />
+          )}
+        </Routes>
+      </HashRouter>
+    </React.StrictMode>
   );
 }
 
